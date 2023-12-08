7a40f4616069db9dc95f83cbf180303d1029f191
Nguyen Nhat Kiem <>
2023-12-08 16:57:37 +0700

    add logging

--- Sources/GPayPackage/GPayPackage.swift
+++ Sources/GPayPackage/GPayPackage.swift
@@ -1,9 +1,70 @@
+import Foundation
 import GalaxyPaySDK
 
-public struct GPayPackage {
-    public private(set) var text = "Hello, World!"
+import OpenTelemetryApi
+import OpenTelemetrySdk
+import StdoutExporter
+import NIO
+import OpenTelemetryProtocolExporterGrpc
+import OpenTelemetryProtocolExporterCommon
+import GRPC
 
-    public init() {
+public class GPayPackage: NSObject {
+    public private(set) var text = "Hello, World!"
+    
+    private static var instance: GPayPackage? = nil
+    public static var shared: GPayPackage {
+        if Self.instance == nil {
+            Self.instance = GPayPackage()
+        }
+        return Self.instance!
+    }
+    
+    let client = ClientConnection.usingPlatformAppropriateTLS(for: MultiThreadedEventLoopGroup(numberOfThreads: 1))
+        .connect(host: "tempo-us-central1.grafana.net", port: 443)
+    
+    var otlpTraceExporter: OtlpTraceExporter
+    
+    let tracer = OpenTelemetry.instance.tracerProvider.get(instrumentationName: "OTel Application", instrumentationVersion: "1.0.0")
+
+    public override init() {
+        print("init GPayPackage")
+        let basicAuth = ""
+        self.otlpTraceExporter = OtlpTraceExporter(
+            channel: client,
+            config: OtlpConfiguration(
+                timeout: OtlpConfiguration.DefaultTimeoutInterval,
+                headers: [
+                    ("Authorization", "Basic \(basicAuth)")
+                ]
+            )
+        )
+        super.init()
+        NotificationCenter.default.addObserver(self, selector: #selector(handlerLogging), name: Notification.Name("logging"), object: nil)
+        
+//         Initialize the tracer provider. The tracer provider is used to expose major API operations, preprocess spans, and configure custom clocks.
+//         Configure the custom rules that are used to generate trace IDs and span IDs and configure custom samplers. You can configure the settings based on your business requirements.
+        let spanExporters = MultiSpanExporter(spanExporters: [StdoutExporter(isDebug: true), otlpTraceExporter])
+        let spanProcessor = BatchSpanProcessor(spanExporter: spanExporters)
+        OpenTelemetry.registerTracerProvider(
+            tracerProvider: TracerProviderBuilder().add(spanProcessor: spanProcessor).with(
+                resource: Resource(
+                    attributes:[
+                        ResourceAttributes.serviceName.rawValue: AttributeValue.string("${service}")
+                    ]
+                )
+            )
+            .with(idGenerator: SDKLogIdGenerator())
+            .build()
+        )
+//        OpenTelemetry.registerPropagators(textPropagators: [TextMapPropagator], baggagePropagator: <#T##TextMapBaggagePropagator#>)
+
+        // Configure other instrumentation collectors based on your business requirements. The following configuration is used to collect data from the NSUrlSession network library.
+//        URLSessionInstrumentation(configuration: URLSessionInstrumentationConfiguration(
+//         shouldInstrument: { request in
+//         return true
+//         })
+//        )
     }
     
     public func showGPWallet(_ userInfo: [String: String], callback: @escaping (Bool,Int,Bool,Bool,Bool)->Void) {
@@ -11,4 +72,69 @@ public struct GPayPackage {
             callback(isInValidData, transactionStatus, isBackFromHomePage, isFlowComplete, isTokenExpired)
         })
     }
+    
+    @objc func handlerLogging(_ notification: Notification) {
+        print("handlerLogging")
+        if let payload = notification.userInfo?["payload"] as? String,
+            let data = payload.toDictionary(),
+            let view = data["view"] as? String, view.trimmingCharacters(in: .whitespacesAndNewlines) != "",
+            let phone = data["Phone"] as? String, phone.trimmingCharacters(in: .whitespacesAndNewlines) != "",
+           let language = data["Language"] as? String, language.trimmingCharacters(in: .whitespacesAndNewlines) != "",
+            let tenant = data["Tenant"] as? String, tenant.trimmingCharacters(in: .whitespacesAndNewlines) != "",
+            let sdkVersion = data["SDK_Version"] as? String, sdkVersion.trimmingCharacters(in: .whitespacesAndNewlines) != "",
+            let env = data["Env"] as? String, env.trimmingCharacters(in: .whitespacesAndNewlines) != "",
+            let xOS = data["X-OS"] as? String, xOS.trimmingCharacters(in: .whitespacesAndNewlines) != ""
+        {
+            let span = self.tracer.spanBuilder(spanName: view).startSpan()
+            span.setAttribute(key: "Phone", value: phone)
+            span.setAttribute(key: "Language", value: language)
+            span.setAttribute(key: "Tenant", value: tenant)
+            span.setAttribute(key: "SDK Version", value: sdkVersion)
+            span.setAttribute(key: "Env", value: env)
+            span.setAttribute(key: "X-OS", value: xOS)
+            span.end()
+        }
+    }
+}
+
+extension String {
+    func toDictionary() -> [String: Any]? {
+        if let data = self.data(using: .utf8) {
+            var jsonObj : Any?
+            do {
+                jsonObj = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
+                return jsonObj as? [String : Any]
+            } catch let error as NSError {
+                print("Failed to convert to NSDictionary:::\(error)")
+                return nil
+            }
+        }
+        
+        return nil
+    }
+}
+
+struct SDKLogIdGenerator: IdGenerator {
+    public init() {}
+
+    public func generateSpanId() -> SpanId {
+        var id: UInt64
+        repeat {
+            id = UInt64.random(in: .min ... .max)
+        } while id == SpanId.invalidId
+        return SpanId(id: id)
+    }
+
+    public func generateTraceId() -> TraceId {
+        let curentDate = Date().getFormattedDate(format: "yyyyMMddHHmmss")
+        return TraceId(fromHexString: curentDate)
+    }
+}
+
+extension Date {
+    func getFormattedDate(format: String) -> String {
+         let dateformat = DateFormatter()
+         dateformat.dateFormat = format
+         return dateformat.string(from: self)
+     }
 }
