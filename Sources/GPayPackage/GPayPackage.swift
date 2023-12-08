8f4d9a9b9006077fbc3cb0378db060061a0425a9
Nguyen Nhat Kiem <>
2023-12-08 16:03:18 +0700

    change call SDK

--- Sources/GPayPackage/GPayPackage.swift
+++ Sources/GPayPackage/GPayPackage.swift
@@ -1,6 +1,14 @@
+import GalaxyPaySDK
+
 public struct GPayPackage {
     public private(set) var text = "Hello, World!"
 
     public init() {
+    }
+    
+    public func showGPWallet(_ userInfo: [String: String], callback: @escaping (Bool,Int,Bool,Bool,Bool)->Void) {
+        GPay.shared.showGPWallet(userInfo, callback: { isInValidData, transactionStatus, isBackFromHomePage, isFlowComplete, isTokenExpired in
+            callback(isInValidData, transactionStatus, isBackFromHomePage, isFlowComplete, isTokenExpired)
+        })
     }
 }
