39af2c40551a1d0e84e1c17872724dafd195bd06
Nguyen Nhat Kiem <>
2024-01-09 10:53:16 +0700

    no message

---
+++ GalaxyPayPod/GalaxyPayPod/GPMain.swift
@@ -0,0 +1,14 @@
+//
+//  GPMain.swift
+//  GalaxyPayPod
+//
+//  Created by Nguyen Nhat Kiem on 09/01/2024.
+//
+
+import Foundation
+
+public class GPMain {
+    public func testPod() {
+        print("Test Pod")
+    }
+}
---
+++ GalaxyPayPod/GalaxyPayPod/GalaxyPayPod.h
@@ -0,0 +1,18 @@
+//
+//  GalaxyPayPod.h
+//  GalaxyPayPod
+//
+//  Created by Nguyen Nhat Kiem on 09/01/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GalaxyPayPod.
+FOUNDATION_EXPORT double GalaxyPayPodVersionNumber;
+
+//! Project version string for GalaxyPayPod.
+FOUNDATION_EXPORT const unsigned char GalaxyPayPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GalaxyPayPod/PublicHeader.h>
+
+
